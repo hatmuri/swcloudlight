c3b743a05adeed4efc2d7edfb874107f5d945c6c
yujung <kimyj4017@gmail.com>
2023-11-22 21:13:45 +0900

    from 링크 및 main 수정

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -1,15 +1,41 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Mainpage.css';
 import menu from './menu.png';
 import main from './main.png';
 
 const Mainpage = () => {
+
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+  const [menu_class, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  const updateMenu=()=>{
+    if(!isMenuClicked){
+      setBurgerClass("burger-bar clicked")
+      setMenuClass("menu visible")
+    }
+    else{
+      setBurgerClass("burger-bar unclicked")
+      setMenuClass("menu hidden")
+    }
+  }
+
   return (
     <div className='blind'>
       <div className='menu'>
         <p>색체, 빛을 일으키다.</p>
-        <img src={menu} alt="menu" />
+        {/* <img src={menu} alt="menu" /> */}
+        <div>
+          <nav>
+            <div className='burger-menu'>
+              <div className={burger_class} onClick={updateMenu}></div>
+              <div className={burger_class} onClick={updateMenu}></div>
+              <div className={burger_class} onClick={updateMenu}></div>
+            </div>
+          </nav>
+          <div className={menu_class}></div>
+        </div>  
       </div>
       <div className='blind2'>
         <p>제 2회 운빛 전시</p>
