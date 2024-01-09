a766dffb8158051854c8326a9d06e1674991280a
Park-HyeonJi <phjgina0314@gmail.com>
2024-01-10 00:35:40 +0900

    제발

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(cors());
 app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: true}));
 
-app.use(express.static(path.join(__dirname,'swcloudlight/build')));
+app.use(express.static(path.join(__dirname,'/swcloudlight/build')));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'swclodlight/build/index.html'));
+    res.sendFile(path.join(__dirname, '/swclodlight/build/index.html'));
     console.log("서버 테스트");
   });
   
