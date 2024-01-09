38710e63fb7a002aad0bc527f12988a57462b3fd
Park-HyeonJi <phjgina0314@gmail.com>
2024-01-10 00:38:07 +0900

    제발

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const mysql = require('mysql2');
 const cors = require('cors');
 const { path } = require('animejs');
+const path = require('path');  // path 모듈 추가
 const PORT = process.env.port || 9000;
 
 const db = mysql.createPool({
@@ -17,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: true}));
 
-app.use(express.static(path.join(__dirname,'/swcloudlight/build')));
+app.use(express.static(path.join(__dirname,'swcloudlight/build')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/swclodlight/build/index.html'));
